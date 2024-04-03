27ebc25ea7776fb78e19f6ea6e64ed6a7c1f4b8f
FER <u20201c131@upc.edu.pe>
2024-04-03 12:41:01 -0500

    feat(sales-order): added sales order item definition.

--- OOPSamble/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSamble/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -1,8 +1,17 @@
-﻿namespace OOPSamble.Sales.Domain.Model.Aggregates;
+﻿using OOPSamble.Shared.Domain.Model.ValueObjects;
+
+namespace OOPSamble.Sales.Domain.Model.Aggregates;
 
 public class SalesOrder(int customerId)
 {
     //elementos basicos que inicializan
     public Guid Id { get; } = Guid.NewGuid(); //solo de lectura  ->Guid permite generar aleatoriamente claves
     public int CustomerId { get; } = customerId;
+    
+    //Crear un status
+    public SalesOrderStatus Status { get; private set; } = SalesOrderStatus.PandingPayment;
+    
+    //se le agrega el address
+    public Address ShippingAddress { get; private set; }
+    public double PaidAmount { get; private set; } = 0;
 }
