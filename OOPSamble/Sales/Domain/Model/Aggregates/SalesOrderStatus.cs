fa37cc08bb6329d690484e662f09a5c1fbb4fd4a
FER <u20201c131@upc.edu.pe>
2024-04-03 11:58:24 -0500

    feat(sales-order): added sales order status definition.

---
+++ OOPSamble/Sales/Domain/Model/Aggregates/SalesOrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPSamble.Sales.Domain.Model.Aggregates;
+
+public enum SalesOrderStatus
+{
+    //Preparo los estados de una orden de ventas
+    Cancelled,
+    PandingPayment,
+    ReadyForShipment,
+    Shipped,
+    Completed
+}
